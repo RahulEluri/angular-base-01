f28167c99d3b616e26bb6209477e87aaff52892a
Eluri Krirshna Sai Rahul <erahul@ariqt.com>
2024-08-29 12:47:38 +0530

    Added ui-password

---
+++ libs/shared/ui-password/src/lib/shared-ui-password.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UiPasswordComponent } from './ui-password/ui-password.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [UiPasswordComponent],
+})
+export class SharedUiPasswordModule {}
---
+++ libs/shared/ui-password/src/lib/ui-password/ui-password.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mars-ui-password',
+  templateUrl: './ui-password.component.html',
+  styleUrl: './ui-password.component.css',
+})
+export class UiPasswordComponent {}
