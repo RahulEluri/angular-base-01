6e43bf732a513de594043dd864672e8a640d5d68
Eluri Krirshna Sai Rahul <erahul@ariqt.com>
2024-08-29 12:30:12 +0530

    working with cbc,added ui-button and moon application with bootstrap

---
+++ libs/shared/ui-button/src/lib/ui-button/ui-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mars-button-ui-button',
+  templateUrl: './ui-button.component.html',
+  styleUrl: './ui-button.component.css',
+})
+export class UiButtonComponent {}
